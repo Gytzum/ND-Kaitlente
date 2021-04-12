83cd98e36c0b9a487c2eff894f84defadc4e239d
Gytzum <gytzum@gmail.com>
2021-04-12 13:29:39 +0300

    ND-Kaitlente-Update

--- JS/main.js
+++ JS/main.js
@@ -1,27 +1,14 @@
-/*----------PLANAS-------------------
-1. sukurti skyles
-2. sukurti knopkes
-3. padaryti knopkes aktyvias click su eventlistener
-a)toggle
-4.padaryti kad skyles reaguotu i paspaudima
-..
-*/
 
-
-//-------Pirma dalis----skyliu sukurimas----
-
-const kaitlenteDOM = document.querySelector('.kaitlente'); //surandame kaitlente
-const buttonsDOM = document.querySelector('.buttons'); //knopkiu tevo radimas
-
-const holes = [1,2,3,4];      // kad sukurtu 4 skyles ir jos turetu unique data pavadinima
-let hole = '';      //kitamasis skylei
-
-for (let i =0; i< holes.length;i++) {   //loop skyliu kurimas
-    hole += `<div class="hole" data-ignite='${holes[i]}'></div>`; //SKyles sukurimas ir data priskirimas
+const kaitlenteDOM = document.querySelector('.kaitlente'); 
+const buttonsDOM = document.querySelector('.buttons'); 
+const holes = [1,2,3,4];     
+let hole = '';     
+for (let i =0; i< holes.length;i++) {   
+    hole += `<div class="hole" data-ignite='${holes[i]}'></div>`; 
 }
-kaitlenteDOM.insertAdjacentHTML('afterbegin', hole); //istatomos skyles i html faila
+kaitlenteDOM.insertAdjacentHTML('afterbegin', hole); 
 
-//----------------ANTRA DALIS -------KNOPKIU SUKURIMAS--------------------
+
 
 const buttons = ['Off','Off','Off','Off'];
 let button = '';
@@ -29,15 +16,11 @@ let button = '';
 for (let i = 0; i < buttons.length; i++) {
     button += `<div class="button" data-btnToggle='${buttons[i]}'>${buttons[i]}</div>`;
 }
-buttonsDOM.insertAdjacentHTML('beforeend', button); // knopkiu ikelimas i html
+buttonsDOM.insertAdjacentHTML('beforeend', button); 
 
-//---------TRECIA DALIS----AKTYVIOS KNOPKES------------------
-// buttonsDOM knopkiu tevas
-// button knopke btnToggle jo data
-//kaitlente skyliu tevas
-//hole skyle   ignite jo data
 
-const skyles = kaitlenteDOM.querySelectorAll('.hole'); //surandame sukurtas skyles
+
+const skyles = kaitlenteDOM.querySelectorAll('.hole'); 
 const knopkes = buttonsDOM.querySelectorAll('.button');
 
 for (let i = 0; i < skyles.length; i++) {
